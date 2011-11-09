7b130fa47420ec873bddaea469e97c5ab367a2e3
Martin Williams <angrygorilla@gmail.com>
2011-11-08 18:26:55 -0800

    got database and listview working

--- Assignment3/src/washington/edu/willi7/GorillaDatabase.java
+++ Assignment3/src/washington/edu/willi7/GorillaDatabase.java
@@ -1,16 +1,71 @@
 package washington.edu.willi7;
 
 import android.content.Context;
+import android.content.res.Resources;
+
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.util.Log;
 
 public class GorillaDatabase extends SQLiteOpenHelper {
 
+	
+	// static column names
+	private final static String NAME_COLUMN = "name";
+	private final static String AGE_COLUMN = "age";
+	private final static String WEIGHT_COLUMN = "weight";
+	private final static String IMAGE_COLUMN = "image";
+	private final static String DESCRIPTION_COLUMN = "description";
+	
+	// private members
+	private final static int mDatabaseVersion = 1;
+	private final static String mDatabaseName = "gorilla";
+	private Context mContext;
+	
+	/** 
+	 * The create table string must create the following columns:
+	 * 
+	 * name varchar (text) 	- name of the gorilla, max length?
+	 * age  integer (int) 	- age of the gorilla
+	 * weight float (float) - weight of the gorilla, atleast 3-4 digits
+	 * image varchar (text) - path or name of image to use, max length?
+	 * descriptions varchar (text) - description about the gorilla.
+	 * 
+	 */
+	private final static String mCreateDatabase = 
+			"CREATE TABLE " + mDatabaseName + "( " +				
+					NAME_COLUMN + " TEXT UNIQUE," + 
+					AGE_COLUMN + " INTEGER," +
+					WEIGHT_COLUMN + " INTEGER NOT NULL," +
+					IMAGE_COLUMN + " TEXT NOT NULL," +
+					DESCRIPTION_COLUMN + " TEXT NOT NULL" + ");";
+	
+	/**
+	 * Constructor
+	 * 
+	 * The database name and version used are the static values internal
+	 * to the class. The parent method is called and the context refernce 
+	 * is set.
+	 *  
+	 * @param context
+	 */
+	public GorillaDatabase(Context context) {
+		super(context, mDatabaseName, null, mDatabaseVersion);
+		
+		// get a reference to the class that created the GorillaDatabase object
+		mContext = context;
+		
+	}
+	
 	public GorillaDatabase(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
 		// TODO Auto-generated constructor stub
+		
+		// can create CursorFactory that returns a different database based off
+		// the name passed in
 	}
 
 	@Override
@@ -21,8 +76,30 @@ public class GorillaDatabase extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
+		db.execSQL(mCreateDatabase);
+		Log.d("Assignment3","onCreate: " + mCreateDatabase);
 		
+		// Get the Resources from the context
+		Resources res = mContext.getResources();
+		
+		// Get the default column values from the strings file
+		String[] name = res.getStringArray(R.array.g_name);
+		String[] image = res.getStringArray(R.array.g_image);
+		String[] description = res.getStringArray(R.array.g_description);
+		int[] age = res.getIntArray(R.array.g_age);
+		int[] weight = res.getIntArray(R.array.g_weight);		
+		
+		for(int i = 0; i < name.length; i++) {
+			
+			String insert = 
+					"INSERT INTO " + GorillaDatabase.mDatabaseName + " (" + 
+			GorillaDatabase.NAME_COLUMN + ", " + GorillaDatabase.IMAGE_COLUMN + ", " + GorillaDatabase.DESCRIPTION_COLUMN + ", " + GorillaDatabase.AGE_COLUMN + ", " + GorillaDatabase.WEIGHT_COLUMN + ") " +
+							
+					" VALUES ('" + name[i] + "', '" + image[i] + "', \"" + description[i] + "\", " + age[i] + ", " + weight[i] + ");";
+			// insert the values
+			db.execSQL(insert);	
+			
+			Log.d("Assignment3","onCreate: " + insert );		}	
 	}
 
 	@Override
@@ -33,8 +110,25 @@ public class GorillaDatabase extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
 		
+		Log.d("Assignment3", "onUpgrade: oldversion: " + oldVersion + " newversion: " + newVersion );
+		db.execSQL("DROP TABLE IF exists gorilla");
+		
+		// create the database
+		this.onCreate(db);
 	}
 
+	
+	public String[] getListViewColumns() {
+		String[] c = { GorillaDatabase.IMAGE_COLUMN, GorillaDatabase.NAME_COLUMN, GorillaDatabase.AGE_COLUMN };
+		return c;
+	}
+	
+	public String getDatabaseName() {
+		return GorillaDatabase.mDatabaseName;
+	}
+	
+	public String getListViewGroupBy() {
+		return GorillaDatabase.NAME_COLUMN;
+	}
 }
--- Assignment3/src/washington/edu/willi7/GorillaListActivity.java
+++ Assignment3/src/washington/edu/willi7/GorillaListActivity.java
@@ -1,14 +1,47 @@
 package washington.edu.willi7;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.ListActivity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class GorillaListActivity extends ListActivity{
 
+	// private data
+	private SQLiteDatabase mDatabase;
+	private CursorAdapter mCursorAdapter;
+ 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		// set the layout
+		setContentView(R.layout.main);
+		
+		// create the database with GorillHelper passing the context (this)
+		GorillaDatabase gdb = new GorillaDatabase(this);
+		mDatabase = gdb.getWritableDatabase();
+		// query the database
+		
+		//Cursor c = mDatabase.query(gdb.getDatabaseName(), gdb.getListViewColumns(), "rowid _id", null, null, null, gdb.getListViewGroupBy());
+		Cursor c = mDatabase.rawQuery("select rowid _id, image, name, age FROM gorilla", null);
+		mCursorAdapter = new RowAdaptor(this, R.layout.row_item, c, gdb.getListViewColumns(), new int[] {R.id.imageView1, R.id.name, R.id.age});
+		
+		// set the list adapter
+		setListAdapter(mCursorAdapter);
 	}
 
 	@Override
@@ -30,4 +63,64 @@ public class GorillaListActivity extends ListActivity{
 	}
 	
 
+	private class RowAdaptor extends SimpleCursorAdapter {
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {			
+			View v = super.getView(position, convertView, parent);
+			
+			// get all recourse references
+			TextView name = (TextView)v.findViewById(R.id.name);
+			TextView age = (TextView)v.findViewById(R.id.age);
+			ImageView icon = (ImageView)v.findViewById(R.id.imageView1);
+			
+			// get the cursor that was passed during construction
+			Cursor c = getCursor();
+			String n = c.getString(c.getColumnIndex("name"));
+			int a = c.getInt(c.getColumnIndex("age"));
+			String f = c.getString(c.getColumnIndex("image"));
+			
+			Log.d("Assignment3","name: " + n + " age: " + a + " image: " + f );
+			
+			name.setText(n);
+			age.setText(Integer.toString(a));
+			String file = f;
+			//name.setText(c.getString(c.getColumnIndex("name")));
+			//age.setText(Integer.toString(c.getInt(c.getColumnIndex("age"))));
+			//String file = c.getString(c.getColumnIndex("image"));
+			
+			// get the bitmap from assets
+			Bitmap bmap = null;
+			InputStream istream = null;
+			
+			try {
+				istream = getAssets().open(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			bmap = BitmapFactory.decodeStream(istream);
+			
+			icon.setImageBitmap(bmap);
+			
+			// return the newly populated view (row)
+			return v;
+		}
+
+		/**
+		 * RowAdapter constructor
+		 * 
+		 * binds the cursor to the row item
+		 * 
+		 * @param context
+		 * @param layout
+		 * @param c
+		 * @param from
+		 * @param to
+		 */
+		public RowAdaptor(Context context, int layout, Cursor c, String[] from,
+				int[] to) {
+			super(context, layout, c, from, to);
+		}
+	}
 }
