98db05dd5c6aedabc1d8b17347b490d01db670fe
Martin Williams <angrygorilla@gmail.com>
2011-11-20 22:17:04 -0800

    added onitemclicked listener. got category switching done. Need to still add message switching.

--- Assignment4/src/washington/edu/willi7/TranslatorDatabase.java
+++ Assignment4/src/washington/edu/willi7/TranslatorDatabase.java
@@ -34,15 +34,18 @@ public class TranslatorDatabase extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 
 		Log.d("assignment4", "onCreate - creating database");
-		String create_category = "CREATE TABLE " + TranslatorColumns.CATEGORY_TABLE + " ("
+		String create_category = "CREATE TABLE "
+				+ TranslatorColumns.CATEGORY_TABLE + " ("
 				+ TranslatorColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
 				+ TranslatorColumns.CATEGORY_COLUMN + " TEXT" + ");";
 
-		String create_language = "CREATE TABLE " + TranslatorColumns.LANGUAGE_TABLE + " ("
+		String create_language = "CREATE TABLE "
+				+ TranslatorColumns.LANGUAGE_TABLE + " ("
 				+ TranslatorColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
 				+ TranslatorColumns.LANGUAGE_COLUMN + " TEXT" + ");";
 
-		String create_message = "CREATE TABLE " + TranslatorColumns.MESSAGE_TABLE + " ("
+		String create_message = "CREATE TABLE "
+				+ TranslatorColumns.MESSAGE_TABLE + " ("
 				+ TranslatorColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
 				+ TranslatorColumns.CATEGORY_COLUMN + " TEXT,"
 				+ TranslatorColumns.MESSAGE_1_COLUMN + " TEXT,"
@@ -70,17 +73,48 @@ public class TranslatorDatabase extends SQLiteOpenHelper {
 		// + ", 'hola');";
 		//
 		// try using cv!
-		addLanguage(db, "English");
-		addLanguage(db, "Spanish");
-		
-		addCategory(db, "Greeting");
-		addCategory(db, "Hospital");
-		addCategory(db, "Resturant");
-	
-		addMessage(db, "Greeting", "Hello, How are you?", "Hola, Como estas?");
-		addMessage(db, "Greeting", "It is nice to meet you!", "Hola, Como estas?");
-		addMessage(db, "Greeting", "Where are you from?", "Hola, Como estas?");
-		addMessage(db, "Greeting", "Do you know what time it is?", "Hola, Como estas?");
+		String[] language_list = mContext.getResources().getStringArray(
+				R.array.language_list);
+		String[] category_list = mContext.getResources().getStringArray(
+				R.array.category_list);
+
+		String[] hospital_message1_list = mContext.getResources()
+				.getStringArray(R.array.hospital_message1_list);
+		String[] hospital_message2_list = mContext.getResources()
+				.getStringArray(R.array.hospital_message2_list);
+
+		String[] city_message1_list = mContext.getResources().getStringArray(
+				R.array.city_message1_list);
+		String[] city_message2_list = mContext.getResources().getStringArray(
+				R.array.city_message2_list);
+
+		String[] restaurant_message1_list = mContext.getResources()
+				.getStringArray(R.array.restaurant_message1_list);
+		String[] restaurant_message2_list = mContext.getResources()
+				.getStringArray(R.array.restaurant_message2_list);
+
+		for (String s : language_list) {
+			addLanguage(db, s);
+		}
+
+		for (String s : category_list) {
+			addCategory(db, s);
+		}
+
+		for (int i = 0; i < hospital_message1_list.length; i++) {
+			addMessage(db, "Hospital", hospital_message1_list[i],
+					hospital_message2_list[i]);
+		}
+
+		for (int i = 0; i < city_message1_list.length; i++) {
+			addMessage(db, "City", city_message1_list[i],
+					city_message2_list[i]);
+		}
+
+		for (int i = 0; i < restaurant_message1_list.length; i++) {
+			addMessage(db, "Restaurant", restaurant_message1_list[i],
+					restaurant_message2_list[i]);
+		}
 	}
 
 	@Override
@@ -97,8 +131,8 @@ public class TranslatorDatabase extends SQLiteOpenHelper {
 		super.onOpen(db);
 	}
 
-	public void addMessage(SQLiteDatabase db, String category,
-			String message1, String message2) {
+	public void addMessage(SQLiteDatabase db, String category, String message1,
+			String message2) {
 
 		Log.d("assignment4", "addMessage - adding messages");
 		ContentValues cv = new ContentValues();
@@ -108,16 +142,14 @@ public class TranslatorDatabase extends SQLiteOpenHelper {
 		db.insert(TranslatorColumns.MESSAGE_TABLE, null, cv);
 	}
 
-	public void addLanguage(SQLiteDatabase db, 
-			String language) {
+	public void addLanguage(SQLiteDatabase db, String language) {
 		Log.d("assignment4", "addLanguage - adding a language");
 		ContentValues cv = new ContentValues();
 		cv.put(TranslatorColumns.LANGUAGE_COLUMN, language);
 		db.insert(TranslatorColumns.LANGUAGE_TABLE, null, cv);
 	}
 
-	public void addCategory(SQLiteDatabase db, 
-			String category) {
+	public void addCategory(SQLiteDatabase db, String category) {
 		Log.d("assignment4", "addCategory - adding a category");
 		ContentValues cv = new ContentValues();
 		cv.put(TranslatorColumns.CATEGORY_COLUMN, category);
@@ -128,21 +160,26 @@ public class TranslatorDatabase extends SQLiteOpenHelper {
 		SQLiteDatabase db = this.getReadableDatabase();
 		// String s = "SELECT DISTINCT " + TranslatorColumns.CATEGORY_COLUMN +
 		// " FROM " + mDatabaseName
-		String s = "SELECT * FROM " + TranslatorColumns.MESSAGE_TABLE + " WHERE "+ TranslatorColumns.CATEGORY_COLUMN + "=?";
+		String s = "SELECT * FROM " + TranslatorColumns.MESSAGE_TABLE
+				+ " WHERE " + TranslatorColumns.CATEGORY_COLUMN + "=?";
 		Cursor c = db.rawQuery(s, new String[] { category });
 		return c;
 	}
 
 	public Cursor queryGetAllCategories() {
 		SQLiteDatabase db = this.getReadableDatabase();
-		String s = "SELECT " + TranslatorColumns._ID + ", " + TranslatorColumns.CATEGORY_COLUMN + " FROM " + TranslatorColumns.CATEGORY_TABLE;
+		String s = "SELECT " + TranslatorColumns._ID + ", "
+				+ TranslatorColumns.CATEGORY_COLUMN + " FROM "
+				+ TranslatorColumns.CATEGORY_TABLE;
 		Cursor c = db.rawQuery(s, null);
 		return c;
 	}
 
 	public Cursor queryGetAllLanguages() {
 		SQLiteDatabase db = this.getReadableDatabase();
-		String s = "SELECT " + TranslatorColumns._ID + ", " + TranslatorColumns.LANGUAGE_COLUMN + " FROM " + TranslatorColumns.LANGUAGE_TABLE;
+		String s = "SELECT " + TranslatorColumns._ID + ", "
+				+ TranslatorColumns.LANGUAGE_COLUMN + " FROM "
+				+ TranslatorColumns.LANGUAGE_TABLE;
 		Cursor c = db.rawQuery(s, null);
 		return c;
 	}
